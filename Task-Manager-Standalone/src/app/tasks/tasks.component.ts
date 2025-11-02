8b43158bd6de1d5f46df66a7d7bdb2cd27eb7298
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-11-02 12:05:59 +0000

    Add Task-Manager-Standalone project used to practice Angular's standalone components

---
+++ Task-Manager-Standalone/src/app/tasks/tasks.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  isAddingTasks = false;
+
+  private tasksService = inject(TasksService);
+
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTasks = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTasks = false;
+  }
+}
